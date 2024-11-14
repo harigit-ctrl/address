e3410e03f81d11d5d20b4198f90984c4fb933c8f
harigit-ctrl <hariprasathcloud@gmail.com>
2024-11-14 22:04:09 +0530

    uploading address web

---
+++ src/script.js
@@ -0,0 +1,36 @@
+
+// function bigImg(x) {
+//     x.style.opacity = 0.5;
+   
+//   }
+  
+//   function normalImg(x) {
+//    x.style.opacity = 1.0;
+//   }
+
+
+function bigImg(x){
+
+  x.src="./main/js-hm-brand-3.webp"
+  // x.style.opacity = 0.5;
+}
+
+function normalImg(x){
+
+  x.src="./main/hm-brand-1.jpeg"
+  // x.style.opacity = 1.0;
+}
+
+
+function secondBigImg(y){
+
+  y.src = "./main/js-brand-3.jpeg";
+}
+
+function secondNormalImg(y){
+
+
+  y.src = "./main/hm-brand-2.webp";
+}
+
+
